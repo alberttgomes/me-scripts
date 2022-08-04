23e265f2111dbdd784728af663f57da697af60da
alberttgomes <albertgomes1995@hotmail.com>
2022-08-04 00:30:59 -0300

    my first script

---
+++ myscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#My first Script ;D
+
+
+AGE=26
+NAME="Albert"
+JOB="Software Engineer Intern"
+echo " ---------------- Hello Community Linux!! ----------------"
+echo "|                                                        |"
+echo "| My name is $NAME have $AGE years and i'm $JOB!         |"
+echo "|                                                        |"
+echo " -------------------------------------------------------  "
+
