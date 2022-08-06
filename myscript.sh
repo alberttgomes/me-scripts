c0849c6baa83d2e7a8f446e3f712fbbfc5811449
alberttgomes <albertgomes1995@hotmail.com>
2022-08-06 20:57:35 -0300

    learning...

--- myscript.sh
+++ myscript.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
-#My first Script ;D
-
+#Author: alberttgomes
+echo "Hi," 
+whoami
+DAY=`date`
+echo "Date: $DAY"
+echo "Current Directory"
+pwd
 
 AGE=26
 NAME="Albert"
 JOB="Software Engineer Intern"
-echo " ------------- Hello Community Linux!! -------------"
-echo "|                                                    |"
-echo "  My name is $NAME have $AGE years and i'm            "
-echo "  $JOB                                                "
-echo "|                                                    |"
-echo " -------------------------------------------------  "
+USER="betinn"
+
+if [ whoami == $USER];
+    then
+        echo " ------------ Hello Community Linux!! ------------"
+        echo "|                                                 |"
+        echo "  My name is $NAME have $AGE years and i'm         "
+        echo "  $JOB                                             "
+        echo "|                                                 |"
+        echo " ------------------------------------------------- "
+    else
+        echo "Invalid!!! Who are you? ;D $whoami"
+fi
+
+if [ whoami ]; then 
+    RESPONSE=""
+    echo "Send the good"
+    read  RESPONSE;
+
+    if [ RESPOSTA != "" ]; 
+        then
+            echo "I don't know what you speak rsrs..."
+        else 
+            echo "You need to write something."
+fi
+
 
