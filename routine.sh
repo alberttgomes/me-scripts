d3c905e7bb9be0bff3a45b0d2fff38ce327f5b77
Albert Gomes <albert.gomes@liferay.com>
2023-02-21 02:38:49 -0300

    Fix variable workspace

--- routine.sh
+++ routine.sh
@@ -2,22 +2,22 @@ function routine () {
     echo "========== Init executable..."
     echo ""
     
-    DIR= "/home/dev/projects/"
+    DIR="/home/dev/projects/"
 
     echo "=========== What project will be today?"
     read PROJECTWORKSPACE
 
     while [[ -z "$PROJECTWORKSPACE" ]]; do 
         echo "Value is null, writing the project valid!"
-        read projectworkspace
+        read PROJECTWORKSPACE
     done
 
     DIR="/home/dev/projects/$PROJECTWORKSPACE"  
 
     if [ -d "$DIR" ]; then
-        echo "'$DIR' found!"
+        echo "'$DIR' Found!"
     else
-        echo "'$DIR' NOT found."
+        echo "'$DIR' Not Found."
     fi  
 
 }
